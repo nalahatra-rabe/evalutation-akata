0f1bdc90435b3c066e1e686c965819c8c5413a9f
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-24 17:51:32 +0300

    feat: test to find min and max

--- 3.js
+++ 3.js
@@ -1,3 +1,31 @@
 function pairProgramming(experiences, mostExperienced) {
-    return[]
+  for (let i = 0; i < experiences.lenght; i++) {
+    if (experiences[i] === mostExperienced) {
+      return [i];
+    }
+  }
+  return [];
 }
+
+console.log(pairProgramming([1, 4, 4, 2, 8, 3], false));
+function findMinMax(numbers) {
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  let min = numbers[0];
+  let max = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < min) {
+      min = numbers[i];
+    }
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+
+  return [min, max];
+}
+
+console.log(findMinMax([1, 4, 4, 2, 8, 3]));
