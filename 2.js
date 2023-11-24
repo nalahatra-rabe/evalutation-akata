9b9b1f3e127aba299f7c6e667f1a3e9e7f8d05f7
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-24 17:26:23 +0300

    feat: test algorithm

--- 2.js
+++ 2.js
@@ -1,4 +1,18 @@
-function nonConsecutiveDigits(N){
-    const J = 0;
-    return J
+function nonConsecutiveDigits(N) {
+  let J = N + 1;
+  let temp = 0;
+  const digits = J.toString().split("").map(Number);
+  console.log(digits);
+  for (let i = 1; i < digits.length; i++) {
+    if (digits[i] === digits[i + 1]) {
+      console.log("mitovy");
+      console.log(J + 2);
+      temp = J + 2;
+    } else {
+      console.log(J + 1);
+    }
+  }
+  return temp;
 }
+
+console.log("Valiny", nonConsecutiveDigits(1765));
