3b396cadaca688726c9e425de589a85e311b0e4c
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-24 17:36:23 +0300

    fix:  undefined response

--- 2.js
+++ 2.js
@@ -2,17 +2,20 @@ function nonConsecutiveDigits(N) {
   let J = N + 1;
   let temp = 0;
   const digits = J.toString().split("").map(Number);
-  console.log(digits);
-  for (let i = 1; i < digits.length; i++) {
-    if (digits[i] === digits[i + 1]) {
-      console.log("mitovy");
-      console.log(J + 2);
-      temp = J + 2;
-    } else {
-      console.log(J + 1);
+
+  if (digits.length === 1) {
+    return J;
+  } else {
+    for (let i = 0; i < digits.length - 1; i++) {
+      if (digits[i] === digits[i + 1]) {
+        temp = J + 2;
+      } else {
+        temp = J + 1;
+        break;
+      }
     }
+    return temp;
   }
-  return temp;
 }
 
-console.log("Valiny", nonConsecutiveDigits(1765));
+console.log("Valiny", nonConsecutiveDigits(98));
