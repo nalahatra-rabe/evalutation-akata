222461e78ba09a085d68324d2d885cc7ff291623
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-24 18:00:47 +0300

    feat: add condition response

--- 3.js
+++ 3.js
@@ -1,31 +1,44 @@
 function pairProgramming(experiences, mostExperienced) {
-  for (let i = 0; i < experiences.lenght; i++) {
-    if (experiences[i] === mostExperienced) {
-      return [i];
-    }
-  }
-  return [];
-}
-
-console.log(pairProgramming([1, 4, 4, 2, 8, 3], false));
-function findMinMax(numbers) {
-  if (numbers.length === 0) {
+  if (experiences.length === 0) {
     return null;
   }
 
-  let min = numbers[0];
-  let max = numbers[0];
+  let min1 = experiences[0];
+  let min2 = experiences[1];
+  let max1 = experiences[0];
+  let max2 = experiences[1];
 
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] < min) {
-      min = numbers[i];
+  let min1Position = 0;
+  let min2Position = 0;
+  let max1Position = 0;
+  let max2Position = 0;
+
+  for (let i = 2; i < experiences.length; i++) {
+    if (experiences[i] < min1) {
+      min2 = min1;
+      min1 = experiences[i];
+      min1Position = i;
+    } else if (experiences[i] < min2) {
+      min2 = experiences[i];
+      min2Position = i;
     }
-    if (numbers[i] > max) {
-      max = numbers[i];
+
+    if (experiences[i] > max1) {
+      max2 = max1;
+      max1 = experiences[i];
+      max1Position = i;
+    } else if (experiences[i] > max2) {
+      max2 = experiences[i];
+      max2Position = i;
     }
   }
 
-  return [min, max];
+  if (!mostExperienced) {
+    return [min1Position, min2Position];
+  } else {
+    return [max1Position, max1Position];
+  }
 }
 
-console.log(findMinMax([1, 4, 4, 2, 8, 3]));
+console.log(pairProgramming([1, 4, 4, 2, 8, 3], false));
+console.log(pairProgramming([1, 4, 4, 2, 8, 3], true));
